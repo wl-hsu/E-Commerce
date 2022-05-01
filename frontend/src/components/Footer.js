a1036845fe83c66f7ca6abc68d54612f3a670e69
wl-hsu <wlhsu.github@gmail.com>
2022-05-01 01:53:51 -0400

    initialize Header and Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+
+const Footer = () => {
+  return <footer>
+      <Container>
+            <Row>
+              <Col className='text-center py-3'>CopyRight &copy; WLH E-Shop</Col>
+            </Row>
+        </Container>
+    </footer>
+}
+
+export default Footer
