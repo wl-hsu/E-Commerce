6a4c194771786545ddc62b159672e760eaedbc66
wl-hsu <wlhsu.github@gmail.com>
2022-05-03 16:03:31 -0400

    add star rating

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -12,9 +13,7 @@ const Product = ({ product }) => {
           <Card.Title>{product.name}</Card.Title>
         </a>
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} ratings {product.numReviews} comments
-          </div>
+          <Rating value={product.rating} text={` ${product.numReviews} comments`} />
         </Card.Text>
         <Card.Text as='h3'> $ {product.price}</Card.Text>
       </Card.Body>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,10 +6,10 @@ import Product from '../components/Product'
 const HomeScreen = () => {
   return (
     <>
-      <h1>New products</h1>
+      <h1>New Product</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
