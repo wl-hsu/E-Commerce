66fa781c5f238900cb302951f12826959eef71a4
wl-hsu <wlhsu.github@gmail.com>
2022-05-04 03:29:23 -0400

    add router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,34 @@
 import React from 'react'
-import {Navbar, Nav, Container} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
-    return (
-      <header>
-        <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-          <Container>
-            <Navbar.Brand href='/'>WLH E-Shop</Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-              <Nav className='ms-auto'>
-                <Nav.Link href='/cart'>
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>WLH E-Shop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
                   <i className='fas fa-shopping-cart'></i>cart
                 </Nav.Link>
-                <Nav.Link href='/login'>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
                   <i className='fas fa-user'></i>login
                 </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    )
-  }
-  
-  export default Header
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
