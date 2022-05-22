82e16c2d39a0feb8c0bf2932eaa8700f170689ac
wl-hsu <wlhsu.github@gmail.com>
2022-05-21 23:33:37 -0400

    add cart page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,12 +5,22 @@ import {
   productListReducer,
   productDeatilsReducer,
 } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDeatilsReducer,
+  cart: cartReducer,
 })
 
-const initialState = {}
+//Get locally stored shopping cart information
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
+
+//initialize state value
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+}
 
 const middleware = [thunk]
 
