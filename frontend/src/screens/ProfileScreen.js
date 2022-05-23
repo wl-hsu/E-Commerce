79ad2a057e76462bd56c2748f63487efed84ff25
wl-hsu <wlhsu.github@gmail.com>
2022-05-23 09:17:48 -0400

    implement update profile

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -3,7 +3,9 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUserDetails } from '../actions/userActions'
+import { USER_UPDATE_PROFILE_RESET } from '../contents/userContents'
+
 
 const ProfileScreen = ({ location, history }) => {
   const [name, setName] = useState('')
@@ -19,27 +21,32 @@ const ProfileScreen = ({ location, history }) => {
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
+  const { success } = userUpdateProfile
+
   useEffect(() => {
     if (!userInfo) {
       history.push('/login')
     } else {
-      if (!user.name) {
-        dispatch(getUserDetails('profile'))
+      if (!user.name || success) {
+        dispatch({ type: USER_UPDATE_PROFILE_RESET })
       } else {
         setName(user.name)
         setEmail(user.email)
       }
     }
-  }, [dispatch, history, userInfo, user])
+  }, [dispatch, history, userInfo, user, success])
   //Form submit function (update user information)
   const submitHandler = (e) => {
     e.preventDefault()
     //dispatch update profile function
+    dispatch(updateUserDetails({ id: user._id, name, email, password }))
   }
   return (
     <Row>
       <Col md={3}>
         <h2>My Profiles</h2>
+        {success && <Message variant='success'>Update successfully</Message>}
         {message && <Message variant='danger'>{message}</Message>}
         {error && <Message variant='danger'>{error}</Message>}
         {loading && <Loader />}
