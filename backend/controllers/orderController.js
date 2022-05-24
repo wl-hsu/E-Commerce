487a8f4db1946efad1edc4b76416314917019695
wl-hsu <wlhsu.github@gmail.com>
2022-05-23 20:54:18 -0400

    add order details page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -33,4 +33,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems }
+//@desc    Get order by order id
+//@route   GET/api/orders/:id
+//@access  private
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'name email'
+    )
+    if (order) {
+      res.json(order)
+    } else {
+      res.status(404)
+      throw new Error('Can\'t find order')
+    }
+  })
+  
+
+  export { addOrderItems, getOrderById }
